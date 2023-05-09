936b5ac0b35aefaf10333daa520099cd12b3cc7d
TheFred94 <frederikrlarsen@gmail.com>
2023-05-09 13:03:48 +0200

    The data comes through from our own api server thingy

--- festival_app/src/pages/_app.js
+++ festival_app/src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+import App from "next/app";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps, navData }) {
+  return (
+    <>
+      <Layout navData={navData}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  // Provide the appContext, in order to do 404's
+  const appProps = await App.getInitialProps(appContext);
+  const res = await fetch("https://scratched-bronze-lingonberry.glitch.me/bands");
+  const navData = await res.json();
+  console.log(navData);
+  return { ...appProps, navData };
+};
