1cac6e42e294ec85ea2d7d37c41df3ecb376fe4e
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-05-11 08:20:29 +0200

    css variables for font and color added

--- booking_flow/tailwind.config.js
+++ booking_flow/tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+
+    colors: {
+      "color-primary1": "#1D1C29",
+      "color-primary2": "#102038",
+      "color-white": "#F9F9F9",
+      "color-secondary1": "#00B88B",
+      "color-secondary2": "#A836DA",
     },
   },
   plugins: [],
-}
+};
--- festival_app/tailwind.config.js
+++ festival_app/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       backgroundImage: {
@@ -9,7 +13,7 @@ module.exports = {
       },
     },
     colors: {
-      hotpink: "#ff69b4",
+      // mycolor: "#ff69b4",
     },
   },
 
