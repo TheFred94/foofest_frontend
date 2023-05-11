deb8d27009849d28d85d6e8edf2b0d0977ec827c
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-05-11 09:18:26 +0200

    variables copied to festival app

--- festival_app/tailwind.config.js
+++ festival_app/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,11 +13,19 @@ module.exports = {
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+
     colors: {
-      hotpink: "#ff69b4",
-      orange: "#ff7849",
+      "color-back": "#1D1C29",
+      "color-blue": "#102038",
+      "color-white": "#F9F9F9",
+      "color-green": "#00B88B",
+      "color-purple": "#A836DA",
+      "color-orange": "#E4A74A",
+    },
+    fontFamily: {
+      "font-display": ["Playfair Display", "serif"],
+      "font-sans": ["Josefin Sans", "sans-serif"],
     },
   },
-
   plugins: [],
 };
