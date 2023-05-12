17e268bbae8673376bd96dcad03fc4e4a0146466
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-05-12 12:39:12 +0200

    modal works

--- booking_flow/src/components/AreaListItem.js
+++ booking_flow/src/components/AreaListItem.js
@@ -31,7 +31,7 @@ export function AreaListItem(props) {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: "background.paper",
+    bgcolor: "#000",
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
@@ -63,10 +63,10 @@ export function AreaListItem(props) {
         <Fade in={open}>
           <Box sx={style}>
             <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
+              Der er ikke nok ledige pladser!
             </Typography>
             <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              For at købe billetter til dette område bedes du justere på antal a billetter
             </Typography>
           </Box>
         </Fade>
