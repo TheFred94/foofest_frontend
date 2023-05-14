b70c571b74808e7f1d16d5f3a5be22f2f0eb71ef
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2023-05-14 20:36:20 +0200

    got local storage test to work

--- festival_app/src/pages/personalprogram.js
+++ festival_app/src/pages/personalprogram.js
@@ -3,13 +3,45 @@ import Anchor from "@/components/Anchor";
 import Button from "@mui/material/Button";
 import Navbar from "@/components/Navbar";
 import { useState, useEffect } from "react";
+import { ButtonBase } from "@mui/material";
 export default function PersonalProgram() {
   const [favorites, setFavorites] = useState();
-  // const [test, setTest] = useEffect();
+  const [sLocalItems, setSLocalItems] = useState([]);
+  const [gLocalItems, setGLocalItems] = useState([]);
 
-  // localStorage.setItem("test", 1);
+  useEffect(() => {
+    const bands = JSON.parse(localStorage.getItem("bands"));
+    if (bands) {
+      setGLocalItems(bands);
+      setSLocalItems(bands);
+    }
+  }, []);
 
-  useEffect;
+  function deleteLocalStorage() {
+    // const bands = JSON.parse(localStorage.getItem("bands"));
+    // if (bands) {
+    //   setGLocalItems(bands);
+    // }
+    localStorage.removeItem("bands");
+    console.log("hey");
+  }
+
+  function addBand(e) {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    console.log(sLocalItems);
+    {
+      sLocalItems.length === 0 ? setSLocalItems([e.target[0].value]) : setSLocalItems([...sLocalItems, e.target[0].value]);
+    }
+    console.log(sLocalItems);
+
+    pushToLocal();
+  }
+
+  function pushToLocal() {
+    console.log(sLocalItems);
+    localStorage.setItem("bands", JSON.stringify(sLocalItems));
+  }
 
   return (
     <>
@@ -17,19 +49,28 @@ export default function PersonalProgram() {
         <title>Personal Program overview</title>
       </Head>
       <Navbar />
-      <main className="h-screen">
-        <h1 className="text-sm color-black">Your personal program</h1>
-        <article className="grid gap-4 grid-cols-2">
-          <div>
-            <p className="color-green">0</p>
-            <Button>Add 1</Button>
-          </div>
-          <div>
-            <p>Noget tekst</p>
-            <Button>Show local Storage</Button>
+      <main className="h-screen bg-color-green p-10">
+        <h1 className="text text-color-black text-center">Your personal program</h1>
+        <article className="grid gap-4 grid-cols-2 items-center justify-items-center m-10">
+          <form onSubmit={addBand}>
+            <label>Add Band</label>
+            <input></input>
+            <ButtonBase type="submit" className="p-2 border-color-yellow border-solid border-2 mt-2">
+              Add band to Local Storage
+            </ButtonBase>
+          </form>
+          <div className="w-full">
+            <ul className="text-color-blue border-color-yellow border-solid border-2 m2">
+              {/* {gLocalItems.length === 0 ? "Nothing in Local Storage" : gLocalItems} */}
+              {gLocalItems.map((band) => (
+                <li key={band}>{band}</li>
+              ))}
+            </ul>
+            <button onClick={() => console.log(sLocalItems)}>See S-LocalS </button>
+            <button onClick={() => console.log(gLocalItems)}>See G-LocalS </button>
+            <button onClick={() => deleteLocalStorage()}>Clear Local Storage! </button>
           </div>
         </article>
-        {/* <button onClick={() => setTest(test + 1)}>Add 1 to </button> */}
         {/* <BandList bands={bands} /> */}
         {/* <Anchor href="/bands">Bands</Anchor> */}
       </main>
