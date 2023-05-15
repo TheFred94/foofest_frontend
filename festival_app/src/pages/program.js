c608e8fc471efabf18d0e2a7961b70774dbe2177
Jachob Westh <jachobwesth@gmail.com>
2023-05-15 15:27:21 +0200

    filter virker på dag og scene

--- festival_app/src/pages/program.js
+++ festival_app/src/pages/program.js
@@ -1,24 +1,29 @@
-import React from "react";
 import Button from "@mui/material/Button";
 import { useState } from "react";
-import Navbar from "@/components/Navbar";
 
 export default function Program({ schedule }) {
   const [selectedStage, setSelectedStage] = useState(null);
+  const [selectedDay, setSelectedDay] = useState(null);
 
   function handleStageClick(stage) {
     setSelectedStage(stage);
   }
+
+  function handleDayClick(day) {
+    setSelectedDay(day);
+    console.log(day);
+  }
   return (
     <div>
       <h1>Program</h1>
-      <Filterbuttons schedule={schedule} onClick={handleStageClick} />
-      <Schedule schedule={schedule} selectedStage={selectedStage} />
+      <FilterbuttonsStage schedule={schedule} onClick={handleStageClick} />
+      <FilterbuttonsDay schedule={schedule} onClick={handleDayClick} />
+      <Schedule schedule={schedule} selectedStage={selectedStage} selectedDay={selectedDay} />
     </div>
   );
 }
 
-function Filterbuttons({ schedule, onClick }) {
+function FilterbuttonsStage({ schedule, onClick }) {
   return (
     <div className="w-screen">
       <Button onClick={() => onClick("")}>All</Button>
@@ -31,7 +36,28 @@ function Filterbuttons({ schedule, onClick }) {
   );
 }
 
-function Schedule({ schedule, selectedStage }) {
+function FilterbuttonsDay({ schedule, onClick }) {
+  const days = new Set();
+
+  Object.keys(schedule).map(stage => {
+    Object.keys(schedule[stage]).map(day => {
+      days.add(day);
+    });
+  });
+
+  return (
+    <div className="w-screen">
+      <Button onClick={() => onClick("")}>All</Button>
+      {[...days].map(day => (
+        <Button key={day} onClick={() => onClick(day)}>
+          {day}
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+function Schedule({ schedule, selectedStage, selectedDay }) {
   return (
     <div className="schedule">
       {Object.keys(schedule)
@@ -39,22 +65,25 @@ function Schedule({ schedule, selectedStage }) {
         .map(stage => (
           <div key={stage}>
             <h2>{stage}</h2>
-            {Object.keys(schedule[stage]).map(day => (
-              <div key={day}>
-                <h3>{day}</h3>
-                {schedule[stage][day].map(timeslot => (
-                  <div key={`${timeslot.start}-${timeslot.end}`}>
-                    <span>{timeslot.start}</span> - <span>{timeslot.end}</span>
-                    <span>{timeslot.act}</span>
-                  </div>
-                ))}
-              </div>
-            ))}
+            {Object.keys(schedule[stage])
+              .filter(day => !selectedDay || day === selectedDay)
+              .map(day => (
+                <div key={day}>
+                  <h3>{day}</h3>
+                  {schedule[stage][day].map(timeslot => (
+                    <div key={`${timeslot.start}-${timeslot.end}`}>
+                      <span>{timeslot.start}</span> - <span>{timeslot.end}</span>
+                      <span>{timeslot.act}</span>
+                    </div>
+                  ))}
+                </div>
+              ))}
           </div>
         ))}
     </div>
   );
 }
+
 export async function getServerSideProps() {
   const api = "http://localhost:8080/schedule";
   const res = await fetch(api);
