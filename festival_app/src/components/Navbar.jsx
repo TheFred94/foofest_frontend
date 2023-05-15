7ddaf9732624b4eab2136d225a86cdb5ffb40eb7
Jachob Westh <jachobwesth@gmail.com>
2023-05-15 15:51:39 +0200

    logo init

--- festival_app/src/components/Navbar.jsx
+++ festival_app/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Image from "next/image";
-import navLogo from "../assets/logoOnly.svg";
+import { NavLogo } from "./svgs";
 import Button from "@mui/material/Button";
 import TemporaryDrawer from "./BurgerMenu";
 
 function Navbar() {
   return (
     <div className="flex justify-between bg-color-black p-3 sticky top-0 w-full z-50">
-      <a className="flex flex-col justify-center">
-        <Image className="place-self-center" src={navLogo} height={75} width={75} alt="shit" />
+      <a className="flex flex-col justify-center max-w-fit">
+        <NavLogo className="max-w-max" />
         <span className="font-font-display font-bold text-xl text-color-white">FOOFEST</span>
       </a>
       <div className="flex justify-center">
