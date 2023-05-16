518a03a09f3fb230a1a994f3b7388fb9f98dd57f
TheFred94 <frederikrlarsen@gmail.com>
2023-05-16 13:28:24 +0200

    cancelled works and has image with blur on background to keep the aspect ratio

--- festival_app/src/pages/bands/[band].jsx
+++ festival_app/src/pages/bands/[band].jsx
@@ -80,7 +80,7 @@ export default function Product({ bandData, scheduleData }) {
       <Head>
         <title>{bandData.name}</title>
       </Head>
-      <div className="relative aspect-video object-contain grid">
+      <div className="relative aspect-video object-contain grid ">
         <Anchor href="/" className="absolute left-5 top-5 z-40">
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#F9F01F" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
             <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
@@ -89,12 +89,13 @@ export default function Product({ bandData, scheduleData }) {
 
         {matchingAct.cancelled === true ? (
           <div className="grid items-center justify-items-center ">
-            <h2 className="w-full text-center bg-color-white z-40 grid col-start-1 row-start-1 text-color-orange">Cancelled</h2>
-            <img src={logoUrl} alt={bandData.bio} className="filter grayscale object-contain w-full col-start-1 row-start-1 aspect-video " />
+            <h2 className="w-full text-center bg-color-white z-40 grid col-start-1 row-start-1 text-color-blue">Cancelled</h2>
+            <img src={logoUrl} alt={bandData.bio} className="z-10 filter grayscale object-contain w-full col-start-1 row-start-1 aspect-video " />
           </div>
         ) : (
-          <img src={logoUrl} alt={bandData.bio} className="w-full aspect-video object-contain" />
+          <img src={logoUrl} alt={bandData.bio} className="w-full aspect-video object-contain z-10" />
         )}
+        <img src={logoUrl} alt={bandData.bio} className="absolute z-0 grid-row-1 w-full aspect-video object-fill blur-sm" />
       </div>
       <h3 className="text-4xl pt-2 pb-3 ">{bandData.name}</h3>
       <section className="pb-5">
