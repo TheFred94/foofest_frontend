7fa58e5342b5e70ebc0b95fe0dfae2af9bdc980f
TheFred94 <frederikrlarsen@gmail.com>
2023-05-19 21:14:48 +0200

    Accordians og forms virker. Issues med submit da knap ikke ligger inde i form

--- booking_flow/src/pages/contact_information.js
+++ booking_flow/src/pages/contact_information.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
+import Accordion from "@mui/material/Accordion";
+import MuiAccordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 function Contact() {
   const [contactForms, setContactForms] = useState([]);
@@ -21,15 +27,29 @@ function Contact() {
     setContactForms(forms);
   }, []);
 
-  return <form>{contactForms}</form>;
+  return (
+    <>
+      <form>{contactForms}</form>
+      <Button className=" rounded-none border-2 border-solid place-self-center border-color-yellow h-10 mb-10 px-6 text-color-yellow hover:bg-color-yellow hover:text-color-black font-sans font-semibold gap-5 ">
+        <span className="pt-1">Go to payment</span>
+      </Button>
+    </>
+  );
 }
 
 function ContactForm() {
   return (
     <>
-      <TextField className="bg-color-white" required id="outlined-required" label="Full Name" variant="filled" />
-      <TextField className="bg-color-white" required id="outlined-required" label="Email" variant="filled" />
-      <TextField className="bg-color-white" required id="outlined-required" label="Phone" variant="filled" />
+      <Accordion>
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography className="text-color-black">Collapsible Group Item </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <TextField className="bg-color-white" required id="outlined-required" label="Full Name" variant="filled" />
+          <TextField className="bg-color-white" required id="outlined-required" label="Email" variant="filled" />
+          <TextField className="bg-color-white" required id="outlined-required" label="Phone" variant="filled" />
+        </AccordionDetails>
+      </Accordion>
     </>
   );
 }
