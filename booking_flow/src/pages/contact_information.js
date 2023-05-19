b950ef27d19b5cc5534776f5497a7daa0df59292
TheFred94 <frederikrlarsen@gmail.com>
2023-05-19 20:56:13 +0200

    divs in a form?

--- booking_flow/src/pages/contact_information.js
+++ booking_flow/src/pages/contact_information.js
@@ -4,28 +4,32 @@ import TextField from "@mui/material/TextField";
 function Contact() {
   const [contactForms, setContactForms] = useState([]);
 
+  // We use useEffect to avoid hydration issues and discrepancies between client and serverside rendering of the form
   useEffect(() => {
     const randomNumber = Math.floor(Math.random() * 10);
     const forms = [];
 
     for (let i = 0; i < randomNumber; i++) {
-      forms.push(<ContactForm key={i} />);
+      forms.push(
+        <div>
+          <ContactForm key={i} />
+        </div>
+      );
     }
 
+    // We use the setContactForms function to update the state and store the generated forms
     setContactForms(forms);
   }, []);
 
-  return <>{contactForms}</>;
+  return <form>{contactForms}</form>;
 }
 
 function ContactForm() {
   return (
     <>
-      <form>
-        <TextField className="bg-color-white" required id="outlined-required" label="Full Name" variant="filled" />
-        <TextField className="bg-color-white" required id="outlined-required" label="Email" variant="filled" />
-        <TextField className="bg-color-white" required id="outlined-required" label="Phone" variant="filled" />
-      </form>
+      <TextField className="bg-color-white" required id="outlined-required" label="Full Name" variant="filled" />
+      <TextField className="bg-color-white" required id="outlined-required" label="Email" variant="filled" />
+      <TextField className="bg-color-white" required id="outlined-required" label="Phone" variant="filled" />
     </>
   );
 }
